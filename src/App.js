e757ad59d2c173799a0cd4af0676e8772c390895
vvicdevv <victoriafarina.dev@gmail.com>
2023-09-25 23:27:08 -0300

    Table created and shown in frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and hello world.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' Element={<Home/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Employees.js
@@ -0,0 +1,16 @@
+const Employees =[
+    {
+        id:"1",
+        Name:"Agus",
+        Age:"21"
+    },{
+        id:"2",
+        Name:"Pala",
+        Age:"22"
+    },{
+        id:"3",
+        Name:"Ayala",
+        Age:"22"
+    }
+]
+export default Employees;
